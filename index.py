61720ff5c8a02a306f96daff10b2a00407d97297
Borko <bstoilov81@gmail.com>
2021-01-16 18:42:10 +0200

    Add connection to sqlite from SQLAlchemy

--- index.py
+++ index.py
@@ -1,7 +1,24 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 import feedparser
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = r'sqlite:///C:\Users\borko\Desktop\flask\foo.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+'''
+from index import db
+db.create_all()
+
+'''
+
+
+class Comments(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30))
+
 
 URL = r'http://feeds.bbci.co.uk/news/rss.xml'
 
@@ -25,10 +42,14 @@ def get_news():
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'GET':
-        return render_template('base.html')
+        result = Comments.query.all()
+        return render_template('base.html', result=result)
     else:
         name = request.form['name']
-        return render_template('base.html', name=name)
+        signiture = Comments(name=name)
+        db.session.add(signiture)
+        db.session.commit()
+        return redirect(url_for('index'))
 
 
 @app.route('/sign/')
