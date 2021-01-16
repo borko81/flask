b5de9aaa4b9737e305f4e878624ef392f085d869
Borko <bstoilov81@gmail.com>
2021-01-16 15:06:15 +0200

    Add sign form, redirect to process page, where enter some name and show in index page

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 import feedparser
 
 app = Flask(__name__)
@@ -6,7 +6,7 @@ app = Flask(__name__)
 URL = r'http://feeds.bbci.co.uk/news/rss.xml'
 
 
-@app.route('/')
+@app.route('/news')
 def get_news():
     parser = feedparser.parse(URL)
     first_article = parser['entries'][0]
@@ -20,6 +20,30 @@ def get_news():
     </body>
     </html>
     """
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'GET':
+        return render_template('base.html')
+    else:
+        name = request.form['name']
+        return render_template('base.html', name=name)
+
+
+@app.route('/sign/')
+def sign():
+    return render_template('sign.html')
+
+
+@app.route('/process', methods=['POST'])
+def process():
+    name = request.form['name']
+    password = request.form['password']
+    if name == 'borko' and password == 'test':
+        return render_template('comments.html')
+    else:
+        return redirect(url_for('sign'))
 
 
 if __name__ == '__main__':
